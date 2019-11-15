8d51efdaa2396d39b904b35f2415281032042bac
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-15 10:44:48 -0700

    characterslist.js: created a link to home and positioned it

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios'
-import styled from 'styled-components'
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom';
 
 import CharacterCard from './CharacterCard';
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const CharListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-`
+`;
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState([])
+export default function CharacterList () {
+    const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get('https://rickandmortyapi.com/api/character/')
-        .then(re => {
-          console.log('response',re);
-          setCharacters(re.data.results)
-        })
-  }, []);
+    useEffect(() => {
 
-  return (
-    <section className="character-list">
-      <CharListWrapper>
-      {characters.map((character, index) =>  <CharacterCard character={character} key={index}/>)}
-      </CharListWrapper>
-    </section>
-  );
+        axios.get('https://rickandmortyapi.com/api/character/')
+            .then(re => {
+                console.log('response', re);
+                setCharacters(re.data.results);
+            });
+    }, []);
+
+    return (
+        <section className="character-list">
+            <NavWrapper>
+                <NavLink to='/'>Home</NavLink>
+            </NavWrapper>
+            <CharListWrapper>
+                {characters.map((character, index) => <CharacterCard character={character} key={index}/>)}
+            </CharListWrapper>
+        </section>
+    );
 }

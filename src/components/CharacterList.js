0af087bbd880699d8ac400d284b2b535cdd57e09
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-15 11:47:05 -0700

    characterlist.js: reformated code to be searchable and added search form

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import axios from 'axios';
 import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
+import SearchForm from './SearchForm';
 
 import CharacterCard from './CharacterCard';
 
@@ -16,23 +17,32 @@ const CharListWrapper = styled.div`
   justify-content: space-evenly;
 `;
 
+
 export default function CharacterList () {
     const [characters, setCharacters] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
-
         axios.get('https://rickandmortyapi.com/api/character/')
             .then(re => {
                 console.log('response', re);
-                setCharacters(re.data.results);
+                const searchedCharacters = re.data.results.filter(character => {
+                    return character.name.toLowerCase().includes(search.toLowerCase());
+                });
+                setCharacters(searchedCharacters);
             });
-    }, []);
+    }, [search]);
+
+    const handleChange = event => {
+        setSearch(event.target.value);
+    };
 
     return (
         <section className="character-list">
             <NavWrapper>
                 <NavLink to='/'>Home</NavLink>
             </NavWrapper>
+            <SearchForm handleChange={handleChange} search={search}/>
             <CharListWrapper>
                 {characters.map((character, index) => <CharacterCard character={character} key={index}/>)}
             </CharListWrapper>

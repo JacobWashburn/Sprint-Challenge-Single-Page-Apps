6d62224673dcf3ab1c7540532b67fa7ae240cf8b
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-15 09:59:02 -0700

    imported characterList to app.js created axios call, set response to state, and mapped over the data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import styled from 'styled-components'
+
+import CharacterCard from './CharacterCard';
+
+const CharListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+        .then(re => {
+          console.log('response',re);
+          setCharacters(re.data.results)
+        })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <CharListWrapper>
+      {characters.map((character, index) =>  <CharacterCard character={character} key={index}/>)}
+      </CharListWrapper>
     </section>
   );
 }

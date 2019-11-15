551cc9f7c72f8412a48e80d842fc44acce7e731f
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-15 09:59:51 -0700

    created character card with a little styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 30%;
+  border: 1px solid red;
+  margin: 3% 0;
+`
+
+const NameH1 = styled.h1`
+  color: blue;
+  text-align: center;
+`;
+const SpeciesH3 = styled.h3`
+  color: red;
+`;
+
+export default function CharacterCard ({character}) {
+  const {name,species} = character
+    return (
+        <CardWrapper>
+        <NameH1>Character: {name}</NameH1>
+          <SpeciesH3>Species: {species}</SpeciesH3>
+          </CardWrapper>
+    );
 }
